b54a8bbfdfed76e53d12d1d32bc27aa9c65743d1
Isaac Guzman <guzmanisaac2005@outlook.com>
2026-07-30 20:23:32 -0500

    feat: historia de usuario

---
+++ CorporateTalentHub/src/main/java/com/mycompany/corporatetalenthub/CorporateTalentHub.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.corporatetalenthub;
+
+/**
+ *
+ * @author cohorte5
+ */
+public class CorporateTalentHub {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
